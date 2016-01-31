1fea78c4bac3ca173403150c3bab9c3814ec4263
MichaelRechenberg <mikerechenberg@owc.net>
2016-01-31 17:05:34 -0600

    Added a tween so we can have one sprite travel to another sprite; the JSON parser will call castSpell on whichever sprites we need to move spells to.  Need to be able to have multiple tweens occur simaltaneously (see: TweenManager)

--- src/webVis/myJavascript.js
+++ src/webVis/myJavascript.js
@@ -1,36 +1,68 @@
 /*
     Michael Rechenberg
 
-    Example Phaser code for mm22
+    Phaser Visualizer code for mm22
 
     This will draw the sprites for each of the characters using
     the JSON from the server (however we decide to send it).
     The server is responsible for making sure the movements are valid.
 
     A good reference for Phaser things: https://leanpub.com/html5shootemupinanafternoon/read
-
     Phaser website: http://phaser.io/
 
+    New JSON will be sent every (1/2) second???
+    
+    Loose JSON??? What does this mean exactly.
+
+    I'll have a shit ton of JSON and I'll update the vis at the time
+        at my discretion (popping from a queue).
+
+    
+    TODO: Keep track of asset Lists
+
+    -----MAP----
+    120x120 pixel quadrants, w/ pillars
+    I'll have to fit all of the characters in each quadrant
+    ----/MAP----
+
+
 */
-var GAME_WIDTH = 800;
-var GAME_HEIGHT = 600;
+
+//Number of milliseconds until the next bit of JSON will be processed
+var TIME_TO_NEXT_UPDATE = 500; 
 
 //designates how many a sprite for every unit of movement designated by the JSON
+//This is unecessary if the JSON sends the absolute position of characters
 var MOVEMENT_PIXELS = 5;
 
-//Reference to the core game object
-//TODO: Set Phaser.CANVAS to Phaser.AUTO if not debugging
-var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.CANVAS, '', 
-    { preload: preload, create: create, update: update, render: render});
 
-//used for cursor input, is not needed if client interactivity
-//  is not necessary
-var cursors;
+//Constants to define Phaser's width and height
+
+//Width of the game, where sprites will be drawn
+var GAME_WIDTH = 600;
+//Height of the game
+var GAME_HEIGHT = 600;
+//Width of the space where to display the stats of each player
+var STAT_WIDTH = 400;
+
+
+//Reference to the core game object
+//If you want to use game.debug in the render() function, you need to set
+//  Phaser.AUTO to Phaser.CANVAS (the renderer)
+var game = new Phaser.Game(GAME_WIDTH + STAT_WIDTH, GAME_HEIGHT, Phaser.AUTO, '', 
+    { preload: preload, create: create, update: update});
 
 //Variables to refer to the characters by
 var star;
 var dude1;
 var dude2;
+
+//Vars to refer to spells
+var spell1;
+var spell2;
+
+//Tween variable
+var tween;
 
 
 //dummy JSON data for updating the location of the characters
@@ -67,6 +99,7 @@ function preload () {
     game.load.image('star', 'assets/20x20star.png');
     game.load.image('dude1', 'assets/dude1.png');
     game.load.image('dude2', 'assets/dude2.png');
+    game.load.image('spell1', 'assets/spell1.png');
     
     
 }
@@ -78,10 +111,11 @@ function create () {
     star = game.add.sprite(game.world.centerX, game.world.centerY, 'star');
     dude1 = game.add.sprite(550, 450, 'dude1');
     dude2 = game.add.sprite(200, 100, 'dude2');
+    spell1 = game.add.sprite(-20, -20, 'spell1');
   
 }
 
-//call every frame, roughly 60 frames per second?
+//call every frame, roughly 60 frames per second
 function update () {
 
 
@@ -117,11 +151,11 @@ function update () {
 //If you delete this function (it is not needed for the game to work)
 //  then you must remove the 'render' key/value from the core game
 //  object's constructor
-function render(){
-    console.log("star is at x: " + star.x + ", y: ", star.y);
-    console.log("dude1 is at x: " + dude1.x + ", y", dude1.y);
-    console.log("dude2 is at x: " + dude2.x + ", y", dude2.y);
-}
+// function render(){
+//     console.log("star is at x: " + star.x + ", y: ", star.y);
+//     console.log("dude1 is at x: " + dude1.x + ", y", dude1.y);
+//     console.log("dude2 is at x: " + dude2.x + ", y", dude2.y);
+// }
 
 
 
@@ -157,4 +191,16 @@ function moveCharacters(){
             locationUpdate[k]["y"] *= -1;
         }
     }
+    //dummy function to show casting spells
+    castSpell(dude2, dude1);
 }
+
+//TODO: Handle multiple tweens simaltaneously
+//This will send a sprite from the caster to the target
+function castSpell(caster, target){
+    spell1.x = caster.x + 20;
+    spell1.y = caster.y + 20;
+    tween = game.add.tween(spell1).to({x: target.x, y: target.y}, TIME_TO_NEXT_UPDATE -10, null, true);  
+}
+
+
