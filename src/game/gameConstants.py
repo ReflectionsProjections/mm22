e8b1f1f56a1f7852705ef0f58071eaccf27c929b
Jactually <samealt001@gmail.com>
2016-02-12 11:41:56 -0600

    added some classes

--- src/game/gameConstants.py
+++ src/game/gameConstants.py
@@ -46,6 +46,24 @@ classesJson = {
             "MovementSpeed" : 5,
             "Abilities"     : [ 0,6,7 ]
     }
+    "paladin" : {
+            "Health"        : 500,
+            "Damage"        : 100,
+            "AttackRange"   : 0,
+            "AttackSpeed"   : 5,
+            "Armor"         : 50,
+            "MovementSpeed" : 5,
+            "Abilities"     : [ 0,3 ]
+    }
+    "sorcerer" : {
+            "Health"        : 500,
+            "Damage"        : 100,
+            "AttackRange"   : 1,
+            "AttackSpeed"   : 5,
+            "Armor"         : 50,
+            "MovementSpeed" : 5,
+            "Abilities"     : [ 0,8 ]
+    }
     "warrior" : {
             "Health"        : 500,
             "Damage"        : 100,
@@ -54,6 +72,15 @@ classesJson = {
             "Armor"         : 50,
             "MovementSpeed" : 5,
             "Abilities"     : [ 0,1 ]
+    }
+    "wizard" : {
+            "Health"        : 500,
+            "Damage"        : 50,
+            "AttackRange"   : 1,
+            "AttackSpeed"   : 5,
+            "Armor"         : 50,
+            "MovementSpeed" : 5,
+            "Abilities"     : [ 0,9,10,11 ]
     }
 }
 
@@ -69,27 +96,39 @@ Defines the abilities
     Range - range of ability in zones
 """
 abilitiesList = [
-    {   #0 suicide- probably never cast this
+    {   #0 burst - break crowd control with a long cooldown
         "StatChanges": [{
             "Target": 0,
-            "Attribute": "Health",
-            "Change": -1000,
-            "Time": 5
+            "Attribute": "Stunned",
+            "Change": False,
+            "Time": 0
+        },
+        {
+            "Target": 0,
+            "Attribute": "Silenced",
+            "Change": False,
+            "Time": 0
+        },
+        {
+            "Target": 0,
+            "Attribute": "Rooted",
+            "Change": False,
+            "Time": 0
         }],
-        "Cooldown"  : 10,
+        "Cooldown"  : 120,
         "Range"     : 0,
     },
-    {   #1 warrior stun (PROBABLY BROKEN)
+    {   #1 melee stun (PROBABLY BROKEN)
         "StatChanges": [{
             "Target": 1,
             "Attribute": "Stunned",
             "Change": True,
-            "Time": 15
+            "Time": 10
         }],
         "Cooldown"  : 60,
         "Range"     : 0,
     },
-    {   #2 assassin armor debuff
+    {   #2 armor debuff
         "StatChanges": [{
             "Target": 1,
             "Attribute": "Armor",
@@ -99,7 +138,7 @@ abilitiesList = [
         "Cooldown"	: 100,
         "Range"		: 0,
     },
-    {   #3 druid heal
+    {   #3 ranged heal
         "StatChanges": [{
             "Target": 2,
             "Attribute": "Health",
@@ -109,7 +148,7 @@ abilitiesList = [
         "Cooldown"  : 50,
         "Range"     : 1,
     },
-    {   #4 druid armor buff
+    {   #4 ranged armor buff
         "StatChanges": [{
             "Target": 2,
             "Attribute": "Armor",
@@ -119,7 +158,7 @@ abilitiesList = [
         "Cooldown"  : 120,
         "Range"     : 1,
     },
-    {   #5 archer attack speed buff (self)
+    {   #5 attack speed buff (self)
         "StatChanges": [{
             "Target": 0,
             "Attribute": "AttackSpeed",
@@ -129,13 +168,8 @@ abilitiesList = [
         "Cooldown"  : 120,
         "Range"     : 0,
     },
-    {   #6 enchanter curse
+    {   #6 ranged damage reduction curse
         "StatChanges": [{
-            "Target": 1,
-            "Attribute": "AttackSpeed",
-            "Change": 2,
-            "Time": 20
-        },
         {
             "Target": 1,
             "Attribute": "Damage",
@@ -145,7 +179,7 @@ abilitiesList = [
         "Cooldown"  : 60,
         "Range"     : 2,
     },
-    {   #6 enchanter buff
+    {   #7 ranged armor and damage buff
         "StatChanges": [{
             "Target": 2,
             "Attribute": "Armor",
@@ -153,13 +187,61 @@ abilitiesList = [
             "Time": 20
         },
         {
-            "Target": 1,
+            "Target": 2,
             "Attribute": "Damage",
             "Change": 20,
             "Time": 20
         }],
         "Cooldown"  : 60,
         "Range"     : 2,
+    },
+    {   #8 sacrifice health for damage (self)
+        "StatChanges": [{
+            "Target": 0,
+            "Attribute": "Health",
+            "Change": -150,
+            "Time": 0
+        },
+        {   "Target": 0,
+            "Attribute": "Damage",
+            "Change": 50,
+            "Time" : 20
+        }],
+        "Cooldown"  : 60,
+        "Range":    : 0,
+    },
+    {   #9 deep freeze
+        "StatChanges": [{
+        {
+            "Target": 1,
+            "Attribute": "Stunned",
+            "Change": True,
+            "Time": 10
+        }],
+        "Cooldown"  : 120,
+        "Range"     : 1,
+    },
+    {   #10 frostbolt
+        "StatChanges": [{
+        {
+            "Target": 1,
+            "Attribute": "Health",
+            "Change": -200,
+            "Time": 0
+        }],
+        "Cooldown"  : 25,
+        "Range"     : 1,
+    },
+    {   #11 icicles
+        "StatChanges": [{
+        {
+            "Target": 1,
+            "Attribute": "Health",
+            "Change": -200,
+            "Time": 0
+        }],
+        "Cooldown"  : 30,
+        "Range"     : 1,
     }
 ]
 
